c52e92eb7d1fc1393b240288ad3ad74a86c3efa3
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-12-06 11:17:59 +0000

    clearout

--- jvm/src/test/scala/riff/DiskMapTest.scala
+++
@@ -1,9 +0,0 @@
-package riff
-
-class DiskMapTest extends BaseTest with DiskTest {
-
-  override def withDisk(callback: Disk.Service => Unit): Unit = {
-    val service = Disk.inMemory("test").value
-    callback(service)
-  }
-}
