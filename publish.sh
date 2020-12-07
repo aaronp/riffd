9aa9a3b226155f90aa432a8044bcc92dcb3c41b0
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-12-07 20:03:17 +0000

    publish

--- publish.sh
+++ publish.sh
@@ -10,6 +10,7 @@ git pull origin gh-pages --rebase
 
 # copy the static site files into the current directory.
 cp -R src/gitbook/_book/* .
+mv src/gitbook/demo .
 
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
