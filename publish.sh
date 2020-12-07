11c6b72fe828164f8aaa3cf333a027928c0e92a7
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-12-07 20:21:29 +0000

    publishscript

--- publish.sh
+++ publish.sh
@@ -2,6 +2,9 @@
 
 source ./makeDoc.sh
 
+rm -rf api
+sbt doc 
+
 # checkout to the gh-pages branch
 git checkout gh-pages
 
@@ -10,21 +13,16 @@ git pull origin gh-pages --rebase
 
 # copy the static site files into the current directory.
 cp -R src/gitbook/_book/* .
-mv src/gitbook/demo .
+#mv src/gitbook/demo .
+mkdir api
+cp -R ./jvm/target/scala-2.13/api* api/
 
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
 git clean -fx src/gitbook/node_modules
 git clean -fx src/gitbook/_book
 
-# add all files
-git add .
-
-# commit
+#git add .
 #git commit -am "Update docs"
-
-# push to the origin
 #git push origin gh-pages
-
-# checkout to the master branch
-#git checkout master
+#git checkout -
